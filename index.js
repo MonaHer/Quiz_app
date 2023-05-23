e511a4b7fdfcf58f83b74204587f3b26404aba8c
Mona Herter <hertermona@gmail.com>
2023-05-23 19:26:29 +0200

    try answer button

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const bodyElement = document.querySelector('[data-js="body"]');
 
 const headerElement = document.querySelector('[data-js="header"]');
@@ -7,9 +6,17 @@ const navbarElement = document.querySelector('[data-js="navbar"]');
 
 const buttonToggle = document.querySelector('[data-js="toggle-button"]');
 
+const output = document.querySelector('[data-js="output"]');
+
 buttonToggle.addEventListener("click", () => {
-    bodyElement.classList.toggle("dark");
-    headerElement.classList.toggle("dark");
-    navbarElement.classList.toggle("dark");
-  });
-  
+  bodyElement.classList.toggle("dark");
+  headerElement.classList.toggle("dark");
+  navbarElement.classList.toggle("dark");
+});
+
+const buttonAnswer = document.querySelector('[data-js="answer-button"]');
+
+const answer = `flexdirection`;
+buttonAnswer.addEventListener("click", () => {
+  output.textContent = answer;
+});
