a423132751707e42e0ee2cf1272a094b460e515e
Mona Herter <hertermona@gmail.com>
2023-05-29 20:06:54 +0200

    add functionality to form

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const bodyElement = document.querySelector('[data-js="body"]');
+/*const bodyElement = document.querySelector('[data-js="body"]');
 
 const headerElement = document.querySelector('[data-js="header"]');
 
@@ -38,10 +38,35 @@ buttonAnswer?.addEventListener("click", () => {
   }
 });
 
-/*if (buttonAnswer.textContent === "Show Answer") {
+if (buttonAnswer.textContent === "Show Answer") {
     buttonAnswer.textContent = "Hide Answer";
   } else {
     buttonAnswer.textContent = "Show Answer";
   }*/
 
-const submitButton = document.querySelector;
+const form = document.querySelector('[data-js="question-form"]');
+const questionInput = document.querySelector('[data-js="question-input"]');
+const answerInput = document.querySelector('[data-js="answer-input"]');
+const tagsInput = document.querySelector('[data-js="tags-input"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const newQuestionCard = document.createElement("section");
+  newQuestionCard.classList.add("question-card");
+  form.append(newQuestionCard);
+
+  const newQuestion = document.createElement("p");
+  newQuestion.classList.add("question");
+  newQuestion.textContent = questionInput.value;
+  newQuestionCard.append(newQuestion);
+
+  const newAnswer = document.createElement("p");
+  newAnswer.textContent = answerInput.value;
+  newQuestion.append(newAnswer);
+
+  const newTags = document.createElement("li");
+  newTags.classList.add("tags");
+  newTags.textContent = tagsInput.value;
+  newAnswer.append(newTags);
+});
