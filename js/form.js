93ec359c00e9a8836ee991ac1c295fd716450112
Mona Herter <hertermona@gmail.com>
2023-06-04 21:36:16 +0200

    change form interactivity

--- js/form.js
+++ js/form.js
@@ -2,13 +2,21 @@ const form = document.querySelector('[data-js="question-form"]');
 const questionInput = document.querySelector('[data-js="question-input"]');
 const answerInput = document.querySelector('[data-js="answer-input"]');
 const tagsInput = document.querySelector('[data-js="tags-input"]');
+const main = document.querySelector('[data-js="main"]');
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const newQuestionCard = document.createElement("section");
   newQuestionCard.classList.add("card");
-  form.append(newQuestionCard);
+  main.append(newQuestionCard);
+
+  const newBookmarkButton = document.createElement("button");
+  newBookmarkButton.setAttribute("type", "button");
+  newBookmarkButton.classList.add("card__bookmark-button");
+  newQuestionCard.append(newBookmarkButton);
+
+  /*icon*/
 
   const newQuestion = document.createElement("p");
   newQuestion.classList.add("card__question");
@@ -19,20 +27,15 @@ form.addEventListener("submit", (e) => {
   newAnswerButton.textContent = "Show Answer";
   newAnswerButton.setAttribute("type", "button");
   newAnswerButton.classList.add("card__answer-button");
-  newQuestion.append(newAnswerButton);
-
-  const newBookmarkButton = document.createElement("button");
-  newBookmarkButton.setAttribute("type", "button");
-  newBookmarkButton.classList.add("card__bookmark-button");
   newQuestionCard.append(newAnswerButton);
 
   const newAnswer = document.createElement("p");
   newAnswer.textContent = answerInput.value;
-  newQuestion.append(newAnswer);
+  newQuestionCard.append(newAnswer);
 
   const newTagsList = document.createElement("ul");
   newTagsList.classList.add("card__tags-list");
-  newQuestion.append(newTagsList);
+  newQuestionCard.append(newTagsList);
 
   const newTags = document.createElement("li");
   newTags.classList.add("card__tags");
